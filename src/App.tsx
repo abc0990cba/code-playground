52e24c7b44904d0afa86760d98c44acd6856b949
max99xam <pepetrov123@list.ru>
2021-07-07 22:07:04 +0300

    added typescript

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,9 @@ import { Route } from "react-router-dom";
 import { Header } from "./components";
 import { Home, Cart } from "./pages";
 
+interface Props { };
 
-function App() {
+const App: React.FC<Props> = () => {
   return (
     <div className="wrapper">
       <Header />
