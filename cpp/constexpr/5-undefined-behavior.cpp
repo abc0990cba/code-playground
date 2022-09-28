ba7085b1c6db1e71474600db1007528a7be59d64
max <kramax42@yandex.by>
2022-09-28 22:00:33 +0300

    commented statements with errors

--- cpp/constexpr/5-undefined-behavior.cpp
+++ cpp/constexpr/5-undefined-behavior.cpp
@@ -8,15 +8,15 @@ int main() {
     constexpr double d1 = 2.0 / 1.0;
 
     // Error: not defined
-    constexpr double d2 = 2.0 / 0.0;
+    // constexpr double d2 = 2.0 / 0.0;
 
     // Error: overflow
-    constexpr int n = std::numeric_limits<int>::max() + 1;
+    // constexpr int n = std::numeric_limits<int>::max() + 1;
 
     int x, y, z[30];
 
     // Error: undefined
-    constexpr auto e1 = &y - &x;
+    // constexpr auto e1 = &y - &x;
 
     // OK
     constexpr auto e2 = &z[20] - &z[3];
@@ -24,7 +24,7 @@ int main() {
     constexpr std::bitset<2> a;
 
     // UB, but unspecified if detected
-    constexpr bool b = a[2];
+    // constexpr bool b = a[2];
 
     return 0;
 }
