03f5c416ef9100d7f29d538d1b008dd3b3980dc5
max <kramax42@yandex.by>
2022-10-06 22:04:10 +0300

    added stream with pipe example node.js

---
+++ nodejs/stream/2-pipe-stream.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+let writeableStream0 = fs.createWriteStream("hello.txt");
+writeableStream0.write("Start\n");
+writeableStream0.write("String\n");
+writeableStream0.write("Finish");
+
+let readableStream = fs.createReadStream("hello.txt", "utf-8");
+
+// Without pipe. 
+let writeableStream1 = fs.createWriteStream("copy1.txt");
+readableStream.on("data", function (chunk) {
+    writeableStream1.write(chunk);
+})
+
+// With pipe.
+let writeableStream2 = fs.createWriteStream("copy2.txt");
+readableStream.pipe(writeableStream2);
