c11a0184b3b13908101fceae83414b1849269fcd
max <kramax42@yandex.by>
2022-10-06 21:42:51 +0300

    updated nodejs fs stream example

--- nodejs/stream/1-fs-stream.js
+++ nodejs/stream/1-fs-stream.js
@@ -3,8 +3,8 @@ const fs = require("fs");
 let writeableStream = fs.createWriteStream("hello.txt");
 
 writeableStream.write("Start!\n");
-for (let i = 1; i <= 1e5; ++i) {
-    writeableStream.write("String.\n");
+for (let i = 1; i <= 1e4; ++i) {
+    writeableStream.write("String string string string string string.\n");
 }
 writeableStream.end("Finish.");
 
@@ -12,8 +12,8 @@ let readableStream = fs.createReadStream("hello.txt", "utf8");
 
 let chunksAmount = 0;
 readableStream.on("data", function (chunk) {
-    console.log("chunk: ", chunk);
     ++chunksAmount;
+    console.log(`chunk ${chunksAmount}: `, chunk);
     console.log("Chunks amount: ", chunksAmount);
 });
 
