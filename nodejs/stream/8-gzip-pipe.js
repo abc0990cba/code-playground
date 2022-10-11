f6f53993c9ec7fd146ce806e25bec2fdf2cde9e6
kramax42 <kramax42@yandex.by>
2022-10-11 23:31:01 +0300

    added node.js http gzip example

--- nodejs/stream/8-gzip-pipe.js
+++ nodejs/stream/8-gzip-pipe.js
@@ -1,5 +1,5 @@
 const fs = require("node:fs");
-const zlib = require("zlib");
+const zlib = require("node:zlib");
 
 const rs = fs.createReadStream("8-gzip-pipe.js", "utf-8");
 const ws = fs.createWriteStream("8-gzip-pipe.js.gz", "utf-8");
---
+++ nodejs/stream/9-http-gzip-pipe.js
@@ -0,0 +1,28 @@
+const fs = require("node:fs");
+const zlib = require("node:zlib");
+const http = require("node:http");
+
+const rs = fs.createReadStream("index.html");
+const gz = zlib.createGzip();
+
+
+const buffers = [];
+let buffer = null;
+
+gz.on("data", (buffer) => {
+    buffers.push(buffer);
+});
+
+gz.on("end", () => {
+    buffer = Buffer.concat(buffers);
+})
+
+rs.pipe(gz);
+
+const server = http.createServer((request, response) => {
+    console.log(request.url);
+    response.writeHead("200", { "Content-Encoding": "gzip" });
+    response.end(buffer);
+})
+
+server.listen(8100);
