99d1b570316d7c96dda598fcc4d5f7eb9c220627
kramax42 <kramax42@yandex.by>
2022-10-11 23:14:27 +0300

    added gzip stream node.js example

---
+++ nodejs/stream/8-gzip-pipe.js
@@ -0,0 +1,12 @@
+const fs = require("node:fs");
+const zlib = require("zlib");
+
+const rs = fs.createReadStream("8-gzip-pipe.js", "utf-8");
+const ws = fs.createWriteStream("8-gzip-pipe.js.gz", "utf-8");
+const gz = zlib.createGzip();
+
+rs.pipe(gz).pipe(ws);
+
+rs.on("end", () => {
+    console.log("Done.");
+})
