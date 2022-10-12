14f1a9ef62968f54d188841daf116be230fa9561
kramax42 <kramax42@yandex.by>
2022-10-12 12:31:16 +0300

    updated js proxy set example

--- js/proxy/2-proxy-set.js
+++ js/proxy/2-proxy-set.js
@@ -2,12 +2,19 @@ let obj = {};
 
 obj = new Proxy(obj, {
     set: function (target, prop, value, receiver) {
-        target[prop] = value;
-        console.log("property set: " + prop + " = " + value);
-        return true;
+        if (typeof value == "number") {
+            target[prop] = value;
+            console.log("property set: " + prop + " = " + value);
+            return true;
+        } else {
+            console.log("property '" + prop + "' must be a number.");
+            return false;
+        }
+
     }
 })
 
 obj.a = 1;
+obj.b = "str";
 
 console.log(obj);
