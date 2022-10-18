491cbb1c3446d06bc2838ec728cb4e20cda5449f
kramax42 <kramax42@yandex.by>
2022-10-18 22:25:42 +0300

    added sql-practice hard level task

--- sql/sql-practice/hard.sql
+++ sql/sql-practice/hard.sql
@@ -192,3 +192,52 @@ SELECT has_insurance,
          FROM admissions
 )
  GROUP BY has_insurance
+
+
+-- Task 6
+-- Show the provinces that has more patients
+-- identified as 'M' than 'F'.
+-- Must only show full province_name
+SELECT province_name
+  FROM patients pa
+  JOIN province_names AS pr ON pa.province_id = pr.province_id
+ GROUP BY pr.province_name
+HAVING COUNT( CASE WHEN gender = 'M' THEN 1 END)
+     > COUNT( CASE WHEN gender = 'F' THEN 1 END);
+-- or
+SELECT province_name
+  FROM (
+       SELECT province_name,
+              SUM(gender = 'M') AS n_male,
+              SUM(gender = 'F') AS n_female
+         FROM patients pa
+         JOIN province_names pr 
+           ON pa.province_id = pr.province_id
+        GROUP BY province_name
+  )
+  WHERE n_male > n_female
+-- or
+SELECT pr.province_name
+  FROM patients AS pa
+  JOIN province_names AS pr
+    ON pa.province_id = pr.province_id
+ GROUP BY pr.province_name
+HAVING SUM(gender = 'M') > SUM(gender = 'F');
+-- or
+SELECT province_name
+  FROM patients p
+  JOIN province_names pr
+    ON p.province_id = pr.province_id
+ GROUP BY province_name
+HAVING SUM(CASE 
+           WHEN gender = 'M' THEN 1
+           ELSE -1 
+           END) > 0
+-- or
+SELECT pr.province_name
+  FROM patients AS pa
+  JOIN province_names AS pr
+    ON pa.province_id = pr.province_id
+ GROUP BY pr.province_name
+HAVING COUNT( CASE WHEN gender = 'M' THEN 1 END)
+     > COUNT(*) * 0.5;
