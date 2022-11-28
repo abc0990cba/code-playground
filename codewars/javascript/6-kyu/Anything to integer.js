01fb055afc792dc896f1a4aa0479e34bbad0559e
Max <kramax42@yandex.by>
2022-11-28 22:24:13 +0300

    added js codewars Anything to integer solution

---
+++ codewars/javascript/6-kyu/Anything to integer.js
@@ -0,0 +1,22 @@
+// 6 kyu
+// Anything to integer
+// https://www.codewars.com/kata/52ac7cad98d1981a840004fc
+
+// Description:
+// Your task is to program a function which converts any input to an integer.
+// Do not perform rounding, the fractional part should simply be discarded.
+// If converting the input to an integer does not make sense (with an object, for instance), the function should return 0 (zero).
+// Also, Math.floor(), parseInt() and parseFloat() are disabled for your unconvenience.
+// Onegaishimasu!
+
+function toInteger(n) {
+  if (isNaN(n)) {
+    return 0;
+  }
+  // The Math.trunc() function returns the integer part of a number by removing any fractional digits.
+  const truncedN = Math.trunc(n);
+  if (!Number.isFinite(truncedN)) {
+    return 0;
+  }
+  return truncedN;  
+}
