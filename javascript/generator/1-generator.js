ac1dfd0519d2a6c9397bff90234dcacbd8e22ea5
Max <kramax42@yandex.by>
2022-11-29 14:19:45 +0300

    updated js generator example

--- javascript/generator/1-generator.js
+++ javascript/generator/1-generator.js
@@ -18,3 +18,11 @@ console.log(res3.value, res3.done); // 3 true
 
 const res4 = generator.next();
 console.log(res4.value, res4.done); // undefined true
+
+const generator2 = generate();
+for(val of generator2) {
+    console.log(val);  // 1, 2 !!! not 3
+}
+
+const arr = [...generate()];
+console.log(arr); // [1, 2]
