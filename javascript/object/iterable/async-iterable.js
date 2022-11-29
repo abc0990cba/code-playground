489adda89389d175e522fe355cc2949a7f2cfffb
Max <kramax42@yandex.by>
2022-11-29 22:10:01 +0300

    added js async iterable object example

---
+++ javascript/object/iterable/async-iterable.js
@@ -0,0 +1,33 @@
+let asyncRange = {
+  from: 1,
+  to: 5,
+
+  [Symbol.asyncIterator]() { 
+    return {
+      current: this.from,
+      last: this.to,
+
+      async next() {
+        await new Promise(resolve => setTimeout(resolve, 1000)); // (3)
+
+        if (this.current <= this.last) {
+          return { 
+              done: false,
+              value: this.current++ 
+              
+          };
+        } else {
+          return {
+              done: true 
+          };
+        }
+      }
+    };
+  }
+};
+
+(async () => {
+  for await (let value of asyncRange) {
+    console.log(value); // 1 ..1sec.. 2 ..1sec.. 3 ..1sec.. 4 ..1sec.. 5
+  }
+})()
