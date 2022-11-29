c10dd5e4bcfe11aacfd08542400b889e6203facd
Max <kramax42@yandex.by>
2022-11-29 22:28:22 +0300

    updated async generator example

--- javascript/generator/4-async-generator.js
+++ javascript/generator/4-async-generator.js
@@ -1,6 +1,6 @@
-async function* generateSequence(start = 1, end = 3) {
+async function* generateSequence(start = 1, end = 3, ms = 1000) {
     for(let i = start; i <= end; ++i) {
-        await new Promise(resolve => setTimeout(resolve, 1000));
+        await new Promise(resolve => setTimeout(resolve, ms));
         yield i;
     }
 }
@@ -9,4 +9,12 @@ async function* generateSequence(start = 1, end = 3) {
     for await(value of generateSequence()) {
         console.log(value); // 1 ..1sec.. 2 ..1sec.. 3
     }
+})()
+
+const generator = generateSequence(4, 6, 500);
+console.log(generator); // Object [AsyncGenerator] {}
+
+(async () => {
+    console.log(await generator.next()); // { value: 4, done: false }
+    console.log(await generator.next()); // { value: 5, done: false }
 })()
