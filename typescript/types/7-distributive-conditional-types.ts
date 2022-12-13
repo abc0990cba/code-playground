f3baed2fe16a1ab476aefb4e5cc8ecb079c3866d
Max <kramax42@yandex.by>
2022-12-13 14:18:56 +0300

    added 7-distributive-conditional-types.ts

---
+++ typescript/types/7-distributive-conditional-types.ts
@@ -0,0 +1,22 @@
+type ToArray<Type> = Type extends any ? Type[] : never;
+type ToArray2<Type> = Type extends unknown ? Type[] : Type[];
+type ToArrayNonDist<Type> = [Type] extends [any] ? Type[] : never;
+
+//  StrArrOrNumArr is string[] | number[]
+type StrArrOrNumArr = ToArray<string | number>;
+
+//  StrArrOrNumArr2 is string[] | number[]
+type StrArrOrNumArr2 = ToArray2<string | number>;
+
+//  StrArrOrNumArr is (string | number)[]
+type StrArrOrNumArr3 = ToArrayNonDist<string | number>;
+
+///////////////////////////////////////////////////////////
+           
+type Without<T, U> = T extends U ? never : T;           
+
+// A is string | number
+type A  = Without<
+  boolean | number | string,
+  boolean
+>
