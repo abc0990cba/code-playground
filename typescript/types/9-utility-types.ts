230a5f79203087be60b8c2fab11b9bee9eeee2a5
Max <kramax42@yandex.by>
2022-12-14 22:37:49 +0300

    update 9-utility-types.ts

--- typescript/types/9-utility-types.ts
+++ typescript/types/9-utility-types.ts
@@ -1,3 +1,5 @@
+// Exclude and Extract
+
 type A = number | string;
 type B = string;
 
@@ -9,6 +11,8 @@ type C1 = Extract<A, B>
 
 /////////////////////////////////////////////
 
+// NonNullable
+
 type R = {a?: number | null};
 
 // RA is number | null | undefined
@@ -19,6 +23,8 @@ type RA1 = NonNullable<R['a']>;
 
 /////////////////////////////////////////////
 
+// ReturnType
+
 type F = (a: number) => string;
 
 // RF is string
@@ -26,8 +32,38 @@ type RF = ReturnType<F>;
 
 /////////////////////////////////////////////
 
+// InstanceType
+
 type G = { new(): H };
 type H = { h: number};
 
 // I is { h: number; }
 type I = InstanceType<G>
+
+type Constructor = new (...args: any[]) => any;
+
+function factory<T extends Constructor>(ctor: T): InstanceType<T> {
+  return new ctor();
+};
+
+class AF { prop = 'simple_prop'; }
+
+const af = factory(AF);
+
+console.log(af); // AF: { "prop": "simple_prop" } 
+
+/////////////////////////////////////////////
+
+// Awaited
+
+// AA is number
+type AA = Awaited<Promise<number>>;
+
+// BB is string
+type BB = Awaited<Promise<Promise<string>>>;
+
+// CC is string | boolean
+type CC = Awaited<string | Promise<Promise<boolean>>>;
+
+// DD is string | number
+type DD = Awaited<Promise<string | Promise<number>>>;
