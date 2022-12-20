fd36ca00b2fd0d5274d33608dd28d80054a461dc
Max <kramax42@yandex.by>
2022-12-20 12:49:31 +0300

    updated distributive types example in TS

--- typescript/types/7-distributive-conditional-types.ts
+++ typescript/types/7-distributive-conditional-types.ts
@@ -1,6 +1,6 @@
 type ToArray<Type> = Type extends any ? Type[] : never;
 type ToArray2<Type> = Type extends unknown ? Type[] : never;
-type ToArrayNonDist<Type> = [Type] extends [any] ? Type[] : never;
+type ToArrayNonDistributive<Type> = [Type] extends [any] ? Type[] : never;
 
 //  StrArrOrNumArr is string[] | number[]
 type StrArrOrNumArr = ToArray<string | number>;
@@ -9,7 +9,7 @@ type StrArrOrNumArr = ToArray<string | number>;
 type StrArrOrNumArr2 = ToArray2<string | number>;
 
 //  StrArrOrNumArr is (string | number)[]
-type StrArrOrNumArr3 = ToArrayNonDist<string | number>;
+type StrArrOrNumArr3 = ToArrayNonDistributive<string | number>;
 
 ///////////////////////////////////////////////////////////
            
