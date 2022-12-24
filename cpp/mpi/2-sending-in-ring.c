c27361c199ff7a3b4340f627720359196b262685
Max <kramax42@yandex.by>
2022-12-25 00:01:09 +0300

    update sending in ring mpi c

--- cpp/mpi/2-sending-in-ring.c
+++ cpp/mpi/2-sending-in-ring.c
@@ -19,6 +19,7 @@ int main(int argc, char** argv) {
 	MPI_Comm_size(MPI_COMM_WORLD, &n_procs);
 	do {
 		if (rank == 0) {
+			// Enter value to send (0 to stop)
 			scanf("%d", &value);
 			MPI_Send(&value, 1, MPI_INT, rank + 1, 0, MPI_COMM_WORLD);
 		} else {
