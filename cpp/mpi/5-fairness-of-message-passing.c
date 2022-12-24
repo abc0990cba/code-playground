2e69659e6414e0c60acda82a72954040f645d7e2
Max <kramax42@yandex.by>
2022-12-25 01:11:29 +0300

    update fairness of message passing

--- cpp/mpi/5-fairness-of-message-passing.c
+++ cpp/mpi/5-fairness-of-message-passing.c
@@ -2,7 +2,7 @@
 // To do this, have all processes except process 0 send 100 messages
 // to process 0. Have process 0 print out the messages as it receives them,
 // using MPI_ANY_SOURCE and MPI_ANY_TAG in MPI_Recv. 
-// Is the MPI implementation fair?
+// Is the MPI implementation fair? No, here!
 
 #define _CRT_SECURE_NO_WARNINGS
 #include "mpi.h"
