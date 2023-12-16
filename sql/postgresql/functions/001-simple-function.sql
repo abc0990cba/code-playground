3f5c3433037a0f1fd49e5f8e12b06a5e34b9a939
Max <kramax42@yandex.by>
2023-12-16 16:03:26 +0300

    Create 001-simple-function.sql
    add simple plpgsql function

---
+++ sql/postgresql/functions/001-simple-function.sql
@@ -0,0 +1,25 @@
+create table employee (
+  id integer primary key,
+  name text not null
+);
+
+insert into employee values (0001, 'Dave');
+insert into employee values (0002, 'Pam');
+insert into employee values (0003, 'Jim');
+
+create function get_all_names(min_id int)
+  returns text
+  language plpgsql
+as
+$$
+  declare names text;
+begin 
+  select string_agg(name, ', ') into names 
+    from employee 
+   where id > min_id;
+
+  return names;
+end;
+$$;
+
+select get_all_names(1);
