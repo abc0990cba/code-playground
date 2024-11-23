251f2beb7df4b4e5a7e1914b4209aa5152d78953
abc0990cba <kramax42@yandex.by>
2024-11-23 14:02:50 +0300

    Create 009-n-senders-m-receivers.go

---
+++ go/channels/009-n-senders-m-receivers.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"log"
+	"math/rand"
+	"strconv"
+	"sync"
+)
+
+func main() {
+	const SendersNum = 500
+	const ReceiversNum = 100
+	const Max = 100
+	const SenderStopVal = 50
+	const ReceiverStopVal = 0
+
+	wg := sync.WaitGroup{}
+	wg.Add(ReceiversNum)
+
+	dataCh := make(chan int)
+	stopCh := make(chan struct{})
+	toStopCh := make(chan string, 1)
+
+	var stoppedBy string
+
+	go func() {
+		stoppedBy = <-toStopCh
+		close(stopCh)
+	}()
+
+	for i := 0; i < SendersNum; i++ {
+		go func(id string) {
+			for {
+				val := rand.Intn(Max)
+
+				if val == SenderStopVal {
+					select {
+					case toStopCh <- "sender#" + id:
+						log.Println("stop val", val)
+					default:
+					}
+
+					return
+				}
+
+				select {
+				case <-stopCh:
+					return
+				default:
+				}
+
+				select {
+				case <-stopCh:
+					return
+				default:
+					dataCh <- val
+				}
+			}
+		}(strconv.Itoa(i))
+	}
+
+	for i := 0; i < ReceiversNum; i++ {
+		go func(id string) {
+			defer wg.Done()
+
+			for {
+				select {
+				case <-stopCh:
+					return
+				default:
+				}
+
+				select {
+				case <-stopCh:
+					return
+				case val := <-dataCh:
+					if val == ReceiverStopVal {
+						select {
+						case toStopCh <- "receiver#" + id:
+							log.Println("stop val", val)
+						default:
+						}
+
+						return
+					}
+					log.Println(val)
+				}
+
+			}
+		}(strconv.Itoa(i))
+	}
+
+	wg.Wait()
+	log.Println("stopped by: ", stoppedBy)
+}
