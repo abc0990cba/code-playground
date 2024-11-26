6daae7257ca2549fadf895d9d04bb356b58bde6b
abc0990cba <kramax42@yandex.by>
2024-11-26 23:27:40 +0300

    Update 002.hash-idx-perf.sql

--- sql/postgresql/indexes/002.hash-idx-perf.sql
+++ sql/postgresql/indexes/002.hash-idx-perf.sql
@@ -58,5 +58,68 @@ BEGIN
 END;
 $$;
 
+------------------------------------------------------------------------------------------------
+
+DO $$
+DECLARE
+  n INTEGER := 100000;
+  duration INTERVAL := 0;
+  start TIMESTAMP;
+  keys TEXT[];
+BEGIN
+    -- Fetch random keys from the table
+    SELECT ARRAY_AGG(key) INTO keys
+      FROM (
+        SELECT key
+          FROM shorturl_hash
+         ORDER BY random()
+         LIMIT n
+      ) AS foo;
+
+    FOR i IN array_lower(keys, 1)..array_upper(keys, 1) LOOP
+        start := clock_timestamp();
+        PERFORM * FROM shorturl_hash WHERE key = keys[i];
+        duration := duration + (clock_timestamp() - start);
+    END LOOP;
+    RAISE NOTICE 'Hash: total=% mean=%', duration, extract('epoch' from duration) / n;
+END;
+$$;
+
+------------------------------------------------------------------------------------------------
+
+DO $$
+DECLARE
+  n INTEGER := 100000;
+  duration INTERVAL := 0;
+  start TIMESTAMP;
+  keys TEXT[];
+
+BEGIN
+  -- Fetch random keys from the table
+  SELECT ARRAY_AGG(key) INTO keys
+    FROM (
+      SELECT key
+        FROM shorturl_btree
+       ORDER BY random()
+       LIMIT n
+    ) AS foo;
+
+    FOR i IN array_lower(keys, 1)..array_upper(keys, 1) LOOP
+      start := clock_timestamp();
+        PERFORM * FROM shorturl_btree WHERE key = keys[i];
+      duration := duration + (clock_timestamp() - start);
+  END LOOP;
+  RAISE NOTICE 'B-Tree: total=% mean=%', duration, extract('epoch' from duration) / n;
+END;
+$$;
+
+------------------------------------------------------------------------------------------------
+
+-- INSERT
+-- Hash: total=00:00:09.764746 mean=9.764746e-06
+-- B-Tree: total=00:00:10.822158 mean=1.0822158e-05
+
+-- SELECT
 -- Hash: total=00:00:00.590032 mean=5.90032e-06
 -- B-Tree: total=00:00:00.923244 mean=9.23244e-06
+
