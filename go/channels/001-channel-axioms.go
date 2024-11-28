2c825e02589138a32ca46fce9f72789d40dea938
abc0990cba <kramax42@yandex.by>
2024-11-28 22:16:07 +0300

    Update and rename 001-do-bad.go to 001-channel-axioms.go

--- go/channels/001-channel-axioms.go
+++ go/channels/001-channel-axioms.go
@@ -16,8 +16,19 @@ func main() {
 	// close(c3)
 	// c3 <- 3
 
-	// 4 - sending to a nil channel cause deadlock
+	// 4 - sending to a nil channel cause deadlock (blocks forever)
 	// c4 := make(chan int)
 	// c4 = nil
 	// c4 <- 4
+
+	// 5 - a receive from nil channel blocks forever
+	// c5 := make(chan int)
+	// c5 = nil
+	// <-c5
+
+	// 6 - a receive from a closed channel returns the zero value immediately
+	// c6 := make(chan int)
+	// close(c6)
+	// a := <-c6
+	// log.Println(a) // 0
 }
