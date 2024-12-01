a219ee77198d6ec500eb515ad069cae53b7c8d63
abc0990cba <kramax42@yandex.by>
2024-12-01 21:49:57 +0300

    Create 001.go

---
+++ leetcode/0121.Best Time to Buy and Sell Stock/go/001.go
@@ -0,0 +1,24 @@
+func maxProfit(prices []int) int {
+    if len(prices) == 0 {
+        return 0
+    }
+
+    l, r := 0, 1
+    var profit int
+
+    for ; r < len(prices); {
+        if prices[l] < prices[r] {
+            tmp := prices[r] - prices[l]
+
+            if tmp > profit {
+                profit = tmp
+            }
+        } else {
+            l = r
+        }
+
+        r++
+    }
+
+    return profit
+}
