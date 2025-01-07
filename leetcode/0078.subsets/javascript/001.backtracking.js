7bd1d7288a502cb2d6bb4de3baa4af63b17efd8e
Кравченко Максим Владимирович <kramax42@yandex.by>
2025-01-07 17:39:55 +0300

    add leetcode solutions

--- leetcode/0078.subsets/javascript/001.backtracking.js
+++ leetcode/0078.subsets/javascript/001.backtracking.js
@@ -6,20 +6,20 @@ var subsets = function(nums) {
   const subsets = [];
 
   const subset = [];
-  const dfs = (idx) => {
+  const backtrack = (idx) => {
     if(idx >= nums.length) {
       subsets.push([...subset]);
       return;
     }
 
     subset.push(nums[idx]);
-    dfs(idx+1);
+    backtrack(idx+1);
 
     subset.pop();
-    dfs(idx+1);
+    backtrack(idx+1);
   }
 
-  dfs(0);
+  backtrack(0);
 
   return subsets;
 };
---
+++ leetcode/0090.subsets-II/javascript/001.backtracking.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsetsWithDup = function(nums) {
+    nums.sort((a, b) => a - b);
+
+    const subsets = [];
+
+    const backtrack = (i, subset) => {
+      subsets.push([...subset]);
+
+      for(let j = i; j < nums.length; j++) {
+        if(j > i && nums[j] === nums[j-1]) {
+          continue;
+        }
+
+        subset.push(nums[j]);
+        backtrack(j+1, subset);
+        subset.pop()
+      }
+    }
+
+    backtrack(0, []);
+
+    return subsets
+};
