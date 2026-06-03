69064824baa4509c55413a403df8c89ba19a5351
abc0990cba <kramax42@yandex.by>
2026-06-03 21:03:26 +0200

    Create 001.reverse-and-transpose.ts

---
+++ leetcode/0048.rotate-image/ts/001.reverse-and-transpose.ts
@@ -0,0 +1,12 @@
+/**
+ Do not return anything, modify matrix in-place instead.
+ */
+function rotate(matrix: number[][]): void {
+    matrix.reverse();
+
+    for(let i = 0; i < matrix.length; i++) {
+        for(let j = i; j < matrix.length; j++) {
+            [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]]
+        }   
+    }
+};
